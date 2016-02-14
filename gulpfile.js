8900e0d57af1724129ea055988f56e5a1f2916f7
misto <mihaistoie@gmail.com>
2016-02-14 11:16:17 +0100

    #

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var shell = require('gulp-shell');
 gulp.task('clean', function () {
     return del([
     'server/',
+    './src/**/*.js',
     './app.js'
   ]);    
    
@@ -31,7 +32,7 @@ gulp.task('upgrade', function (done) {
 
 
 gulp.task('ts', ['clean'], function () {
-    var tsProject = ts.createProject(path.resolve('./src/tsconfig.json'));
+    var tsProject = ts.createProject(path.resolve('./tsconfig.json'));
     var tsResult = gulp.src(path.resolve('./src/**/*.ts')).pipe(ts(tsProject));
     return tsResult.js.pipe(gulp.dest(path.resolve('./')));
   
